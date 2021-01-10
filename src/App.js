d83bbf196ffa68cca13a31bc7fdb754143018a2c
Babatoonday Mattieu <32313099+bbmattieu9@users.noreply.github.com>
2021-01-10 17:25:49 +0100

    [Header] add header content

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
+import Header from './Header';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        
+        {/* Header */}
+        <Header />
+
+        {/* Home */}
       </header>
     </div>
   );
