0311c365337d2522fc3ef8519cdb56cfb5b753f8
Babatoonday Mattieu <32313099+bbmattieu9@users.noreply.github.com>
2021-01-12 00:51:15 +0100

    [Firebase] add firebase file

---
+++ src/firebase.js
@@ -0,0 +1,15 @@
+
+import firebase from 'firebase';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBNSNtT9YB0sgh1VeW7pegCEsiC_E8w4D0",
+    authDomain: "clone-55e12.firebaseapp.com",
+    projectId: "clone-55e12",
+    storageBucket: "clone-55e12.appspot.com",
+    messagingSenderId: "895276360523",
+    appId: "1:895276360523:web:fcc5c93aab6777a51bfece",
+    measurementId: "G-5QC210STCN"
+  };
+
+
+//   export default firebase;
